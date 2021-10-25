900903959f0b6e29f9d785961d44813ce12937e3
MarcoGratacos <msgratacos@gmail.com>
2021-10-25 19:59:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let clubHincha = prompt("ingresar club del que sos hincha");
+
+if ((clubHincha == "boca") || (clubHincha == "racing") || (clubHincha == "independiente"))
+{alert("perdiste la Libertadores 2018");}
+
+
+else if(clubHincha == "river")
+{ alert("Ganaste la Final de Madrid 2018")}
+
+else if(clubHincha ="") 
+{ alert("no ingresaste ningún club")}
